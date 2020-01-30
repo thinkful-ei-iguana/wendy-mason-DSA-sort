b26f568c22c218e2022bef58c8eea0d890204b7e
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-01-30 12:48:34 -0800

    on 5

--- drills.js
+++ drills.js
@@ -1,3 +1,5 @@
+const LL = require("./LinkedList.js");
+
 //1 merge sorting
 
 const data = [21, 1, 26, 45, 29, 28, 2, 9, 16, 49, 39, 27, 43, 34, 46, 40];
@@ -42,7 +44,7 @@ function merge(left, right, array) {
   console.log(array);
   return array; // [1,21] [26,45] [28, 29] [2,9] [16,49] [27,39] [34,43] [40,46]
 } // [1,21,26,45]   [2,9,28,29]   [16,27,39,49]   [34,40,43,46]
-mergeSort(data);
+// mergeSort(data);
 //What is the resulting list that will be sorted after 3 recursive calls to mergesort?   [21]
 //What is the resulting list that will be sorted after 16 recursive calls to mergesort?  [2,9]
 //What are the first 2 lists to be merged?  [1] [21]
@@ -62,12 +64,208 @@ mergeSort(data);
 
 //14, 17, 13, 15, 19, 10, 3, 16, 9, 12
 
-//after the second partitioning
+//last item as pivot
 //1st
 //10, 17, 13, 15, 19, 14, 3, 16, 9, 12
 
 //2nd
 //10, 3, 13, 15, 19, 14, 17, 16, 9, 12
 
+//3rd
 //10, 3, 9, 15, 19, 14, 17, 16, 13, 12
- //            j
+//              j
+
+//first item as pivot
+//14, 17, 13, 15, 19, 10, 3, 16, 9, 12
+//1st
+//13, 17, 14, 15, 19, 10, 3, 16, 9, 12
+//2nd
+//13, 10, 14, 15, 19, 17, 3, 16, 9, 12
+
+//implementation
+
+/*  33,
+45,
+72,
+56,
+44,
+21,
+88,
+27,
+68,
+15,
+62,
+93,
+98,
+73,
+28,
+16,
+46,
+87,
+28,
+65,
+38,
+67,
+16,
+85,
+63,
+23,
+69,
+64,
+91,
+9,
+70,
+81,
+27,
+97,
+82,
+6,
+88,
+3,
+7,
+46,
+13,
+11,
+64,
+76,
+31,
+26,
+38,
+28,
+13,
+17,
+69,
+90,
+1,
+6,
+7,
+64,
+43,
+9,
+73,
+80,
+98,
+46,
+27,
+22,
+87,
+49,
+83,
+6,
+39,
+42,
+51,
+54,
+84,
+34,
+53,
+78,
+40,
+14,
+5
+*/
+const arr = [
+  89,
+  30,
+  25,
+  32,
+  72,
+  70,
+  51,
+  42,
+  25,
+  24,
+  53,
+  55,
+  78,
+  50,
+  13,
+  40,
+  48,
+  32,
+  26,
+  2,
+  14
+];
+function qSort(array, start = 0, end = array.length) {
+  if (start >= end) {
+    console.log(array);
+    return array;
+  }
+  const middle = partition(array, start, end);
+  array = qSort(array, start, middle);
+  array = qSort(array, middle + 1, end);
+  return array;
+}
+
+function partition(array, start, end) {
+  const pivot = array[end - 1];
+  let j = start;
+  for (let i = start; i < end - 1; i++) {
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
+  }
+  swap(array, end - 1, j);
+  return j;
+}
+
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+}
+
+// qSort(arr);
+
+//4
+// mergeSort(arr);
+
+//5
+const newLL = new LL();
+
+newLL.insertLast(8);
+newLL.insertLast(10);
+newLL.insertLast(1);
+newLL.insertLast(5);
+newLL.insertLast(9);
+newLL.insertLast(2);
+newLL.insertLast(4);
+newLL.insertLast(3);
+// console.log(newLL.mergeSort());
+
+// mergeSort(newLL);
+
+
+//Bucket sort
+//
+const bucket = [50,7,17,22,3,48,38,35,40]
+      
+
+//while arr[i] is > low, then swap and i++
+//while arr[j] is < j, then swap and j--
+
+//low = 3, high = 50
+function sort(arr, low, high) {
+  let i = 0;
+  let j = arr.length-1;
+
+  while(j >=i) {
+    while(arr[i] > low && j>i) {
+      i++;
+    }
+    while(arr[j] < high && j > i) {
+      j--;
+    }
+    if(arr[i] > arr[j]) {
+      swap(arr, i, j);
+      i++;
+      j--;
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+sort(bucket);
